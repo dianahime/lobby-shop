cfd7e544724dc658b8b59386a5ac94091231c2bc
jujuhartmann <j.hartmann@me.com>
2020-06-20 14:33:21 +0200

    Add customer registration form

---
+++ src/component/CustomerRegistration.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+export default function CustomerRegistration() {
+  const [name, setName] = useState()
+  const [email, setEmail] = useState()
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    if (name && email) {
+      axios
+        .post('http://localhost:2020/customers', { name, email })
+        .then(() => {
+          setName('')
+          setEmail('')
+          alert('Danke für Ihre Daten. Wir werden sie demnächst verkaufen :P')
+        })
+        .catch((error) => {
+          alert('Sie sind hier nicht willkommen.')
+          console.log(error)
+        })
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        onChange={(event) => setName(event.target.value)}
+        value={name}
+        type="text"
+        placeholder="Ihr werter Name"
+      />
+      <input
+        onChange={(event) => setEmail(event.target.value)}
+        value={email}
+        type="email"
+        placeholder="Ihre elektronische Adresse"
+      />
+      <button>Jetzt registrieren</button>
+    </form>
+  )
+}
