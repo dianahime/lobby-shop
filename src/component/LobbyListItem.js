f2f7b730057b74b241c1d6da65e1f8a517cd71da
dianahime <dajana.pavlovic26@gmail.com>
2020-06-20 21:15:28 +0200

    Add alt tag

--- src/component/LobbyListItem.js
+++ src/component/LobbyListItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function LobbyListItem({ lobbyist }) {
   return (
     <li>
-      <img src={lobbyist.imageURL}></img>
+      <img src={lobbyist.imageURL} alt={`Ein Bild von ${lobbyist.name}`}></img>
       <div className="headline-container">
         <div className="lobbyist-name">{lobbyist.name}</div>
         <div className="lobbyist-price">{lobbyist.price}</div>
