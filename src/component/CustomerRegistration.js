bc6fd1bddb342f10a9ddcb1584d2a024204808f7
danielsilvayanez <daniel.silva@xing.com>
2020-06-26 16:52:56 +0200

    Changed customer registration form and tried to add Formik and yup

--- src/component/CustomerRegistration.js
+++ src/component/CustomerRegistration.js
@@ -1,11 +1,54 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import * as Yup from 'yup'
+import { Formik, Form, Field, ErrorMessage, ErrorStyles } from 'formik'
+
+let ProfileSchema = Yup.object().shape({
+  name: Yup.string().required(),
+  email: Yup.string()
+    .email('DAS IST KEINE GUELTIGE EMAIL ADRESSE!!!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+})
 
 export default function CustomerRegistration({ onRegistration }) {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
+  return (
+    <Formik
+      initialValues={{ name: '', email: '' }}
+      validationSchema={ProfileSchema}
+      onSubmit={(values, { setSubmitting }) => {
+        axios
+          .post('http://localhost:2020/customers', { name, email })
+          .then(() => {
+            alert('Danke für Ihre Daten. Wir werden sie demnächst verkaufen :P')
+            onRegistration(setSubmitting(values))
+          })
+          .catch((error) => {
+            alert('Sie sind hier nicht willkommen.')
+            console.log(error)
+          })
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2))
+          setSubmitting(false)
+        }, 400)
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form className="formik">
+          <Field type="name" name="name" />
+          <ErrorMessage name="name" component="div" />
+          <Field type="email" name="email" />
+          <ErrorMessage name="email" component="div" />
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
+  )
+}
 
-  const handleSubmit = (event) => {
+/*  const handleSubmit = (event) => {
     event.preventDefault()
     if (name && email) {
       axios
@@ -21,23 +64,6 @@ export default function CustomerRegistration({ onRegistration }) {
           console.log(error)
         })
     }
-  }
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        value={name}
-        onChange={(event) => setName(event.target.value)}
-        type="text"
-        placeholder="Ihr werter Name"
-      />
-      <input
-        value={email}
-        onChange={(event) => setEmail(event.target.value)}
-        type="email"
-        placeholder="Ihre elektronische Adresse"
-      />
-      <button>Jetzt betreten</button>
-    </form>
-  )
-}
+    <ErrorStyles name="email" component="div" />
+  }*/
